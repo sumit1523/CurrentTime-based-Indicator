960f914445f4610fbd0838104ff63858dfd132f5
sumit kumar <34701534+sumit1523@users.noreply.github.com>
2019-07-22 19:57:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import TimeIndicator from './TimeIndicator';
+import StoreOpenTime from './StoreOpenTime';
 function App() {
   return (
     <div className="App">
       <TimeIndicator />
+      <StoreOpenTime />
     </div>
   );
 }
