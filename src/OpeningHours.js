20c0f3c1117c373e13b340e1bf6fe074a702370f
sumit kumar <sumit1523@gmail.com>
2019-08-02 01:53:24 +0530

    code refactor

--- src/OpeningHours.js
+++ src/OpeningHours.js
@@ -8,28 +8,19 @@ const OpeningHours = () => {
         }
     }
     const getOpeningTime = () => {
-        const open = '08:30'; // Time must me  HH:MM
-        const close = '20:10'; // Time must me  HH:MM
-        
+        const open = '01:30'; // Time must me  HH:MM
+        const close = '01:52'; // Time must me  HH:MM
+
         const currentDate = new Date();
         let currentTimeHours = currentDate.getHours() < 10 ? "0" + currentDate.getHours() : currentDate.getHours();
         let currentTimeMinutes = currentDate.getMinutes() < 10 ? "0" + currentDate.getMinutes() : currentDate.getMinutes();
         const currentTime = currentTimeHours + "." + currentTimeMinutes;
-        console.log("current Time - " + currentTime);
 
         const openTime = open.split(":")[0] + "." + open.split(":")[1];
-        console.log("open Time - " + openTime);
 
         const closeTime = close.split(":")[0] + "." + close.split(":")[1];
-        console.log("close Time - " + closeTime);
 
-        if (currentTime >= openTime && currentTime <= closeTime) {
-            console.log("OPEN");
-            return true;
-        } else {
-            console.log("CLOSE");
-            return false;
-        }
+        return currentTime >= openTime && currentTime <= closeTime ? true : false;
     }
     const getColorStatus = () => getOpeningTime() ? GET_STATUS.COLOR.GREEN : GET_STATUS.COLOR.RED
 
